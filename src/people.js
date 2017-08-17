df4dff2f2750aac4fdf5087a4ea92a9c48a057dd
Jie ren Tian <110873204@qq.com>
2017-08-17 11:47:15 +0800

    Webpack..

---
+++ src/people.js
@@ -0,0 +1,16 @@
+// src/people.js
+const people = [{
+    manager: 'Jen',
+    name: 'Bob'
+}, {
+    manager: 'Jen',
+    name: 'Sue'
+}, {
+    manager: 'Bob',
+    name: 'Shirley'
+}, {
+    manager: 'Bob',
+    name: 'Terrence'
+}];
+
+export default people;
